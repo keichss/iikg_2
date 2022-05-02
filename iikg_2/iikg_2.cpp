6a37adc1fc24652f1f446960c1370773b727c53f
Екатерина Челышкина <chelyshkinaekaterina@gmail.com>
2022-05-02 20:48:12 +0500

    изменения матриц

--- iikg_2/iikg_2.cpp
+++ iikg_2/iikg_2.cpp
@@ -44,7 +44,7 @@ static void RenderSceneCB()
     Matrix4f World; // подготовка матрицы 4 на 4
 
     World.m[0][0] = 1.0f; World.m[0][1] = 0.0f; World.m[0][2] = 0.0f; World.m[0][3] = sinf(Scale);
-    World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
+    World.m[1][0] = 0.0f; World.m[1][1] = 1.0f; World.m[1][2] = 0.0f; World.m[1][3] = cosf(Scale);
     World.m[2][0] = 0.0f; World.m[2][1] = 0.0f; World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
     World.m[3][0] = 0.0f; World.m[3][1] = 0.0f; World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
 
--- iikg_2_1/iikg_2_1.cpp
+++ iikg_2_1/iikg_2_1.cpp
@@ -38,14 +38,14 @@ static void RenderSceneCB()
 
     static float Scale = 0.0f;
 
-    Scale += 0.005f;
+    Scale += 0.001f;
 
     Matrix4f World; //задаем оси вращения
 
-    World.m[0][0] = cosf(Scale); World.m[0][1] = -sinf(Scale); World.m[0][2] = 0.0f; World.m[0][3] = 0.0f;
-    World.m[1][0] = 0.0f; World.m[1][1] = cosf(Scale);  World.m[1][2] = 0.0f; World.m[1][3] = 0.0f;
-    World.m[2][0] = 0.0f;        World.m[2][1] = 0.0f;         World.m[2][2] = 1.0f; World.m[2][3] = 0.0f;
-    World.m[3][0] = 0.0f;        World.m[3][1] = 0.0f;         World.m[3][2] = 0.0f; World.m[3][3] = 1.0f;
+    World.m[0][0] = 1.0f; World.m[0][1] = 0.0f;         World.m[0][2] = 0.0f;           World.m[0][3] = 0.0f;
+    World.m[1][0] = 0.0f; World.m[1][1] = cosf(Scale);  World.m[1][2] = -sinf(Scale);   World.m[1][3] = 0.0f;
+    World.m[2][0] = 0.0f; World.m[2][1] = sinf(Scale);  World.m[2][2] = cosf(Scale);    World.m[2][3] = 0.0f;
+    World.m[3][0] = 0.0f; World.m[3][1] = 0.0f;         World.m[3][2] = 0.0f;           World.m[3][3] = 1.0f;
 
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
 
